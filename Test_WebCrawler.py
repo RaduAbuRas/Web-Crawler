0deab775cb6d9b5f51973770ca45f5c814a0b77d
TheTugaOne <raduradu0410@yahoo.com>
2019-12-11 22:11:29 +0200

    Update Test_WebCrawler.py

--- Test_WebCrawler.py
+++ Test_WebCrawler.py
@@ -6,7 +6,7 @@ import WebCrawler
 class TestWebCrawl(unittest.TestCase):
 
     def test_crawlPageThreeLevels(self):
-        WebCrawler.crawlPage('https://legioxiii.ro', 2)
+        WebCrawler.crawlPage('https://legioxiii.ro', 1)
 
 if __name__ == '__main__':
     unittest.main()
